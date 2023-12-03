05633b7b72a18f97847f2b3a4d7ef6c5e2f11115
高琪 <gaoqi198@163.com>
2023-12-04 00:49:22 +0800

    feat: video_segment by length

--- video_seg.py
+++ video_seg.py
@@ -6,8 +6,12 @@ from subprocess import check_call
 filename = "./videos/1.mp4"
 start = "00:00:00"
 end = "00:00:10"
-output = "./output/segment.mp4"
-cmd = "ffmpeg -i {0} -c:v libx264 -crf 18 -ss {1} -to {2} {3}".format(filename, start, end, output)
+length = 10
+output1 = "./output/segment_1.mp4"
+output2 = "./output/segment_2.mp4"
+cmd1 = "ffmpeg -i {0} -c:v libx264 -crf 18 -ss {1} -to {2} {3}".format(filename, start, end, output1)
+cmd2 = "ffmpeg -i {0} -c:v libx264 -crf 18 -ss {1} -t {2} {3}".format(filename, start, length, output2)
 if filename.endswith(".mp4"):
-    data = os.system(cmd)
-    print(data)
+    data1 = os.system(cmd1)
+    data2 = os.system(cmd2)
+    print(data1, data2)
